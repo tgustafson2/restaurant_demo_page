726b6cd19c974c0fcd42a78b173bc42d451b4eb5
Thomas Gustafson <gus.duckfan@gmail.com>
2024-04-11 21:26:09 -0700

    Adds rendering for home content using js modules
     - Adds basic html elements for home page(css still needed)
    
     - Adds event listeners for button navigation

---
+++ src/homePage.js
@@ -0,0 +1,42 @@
+
+const homePage = document.createElement('div');
+homePage.className = "home";
+
+const restaurantName = document.createElement('h1');
+restaurantName.className = "restaurant-name";
+restaurantName.textContent = "Scratch Home Kitchen";
+
+const promoPicture = document.createElement('div');
+promoPicture.classList.add("card");
+promoPicture.classList.add("promo-picture-container");
+
+const picture = document.createElement('img');
+picture.src = "./skjf.png";
+promoPicture.appendChild(picture);
+
+const descriptionContainer = document.createElement('div');
+descriptionContainer.classList.add("card");
+const quote = document.createElement('p');
+quote.innerText = "Incredible tasting assortment from cuisines across the globe with an Italian focus. Have a taste of Mexican,"
+                                                                +"Asian, Italian, or American. With one of my wife's signature dishes made from scratch";
+const testimonialGiver = document.createElement('h5');
+testimonialGiver.innerText = "Loving Husband";
+descriptionContainer.appendChild(quote);
+descriptionContainer.appendChild(testimonialGiver);
+
+
+
+homePage.appendChild(restaurantName);
+homePage.appendChild(promoPicture);
+homePage.appendChild(descriptionContainer);
+
+
+
+
+
+function getHomePage(){
+    return homePage;
+    
+}
+
+export { getHomePage }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-const container = document.createElement('div');
-container.classList.add("page-container");
+import {getHomePage} from "./homePage";
 
-document.body.appendChild(container);
+const content = document.querySelector("#content");
+const homeBtn = document.querySelector("#home");
+const menuBtn = document.querySelector("#menu");
+const aboutBtn = document.querySelector("#about");
+
+homeBtn.addEventListener('click', event =>{
+    content.replaceChildren();
+    content.append(getHomePage());
+})
+
+menuBtn.addEventListener('click', event =>{
+    content.replaceChildren();
+    // content.append(getMenuPage());
+})
+
+aboutBtn.addEventListener('click', event =>{
+    content.replaceChildren();
+    // content.append(getAboutPage());
+})
+
+content.append(getHomePage());
+
